27824640ec3f2aaf12ee4825c9493fdc63f30425
Leon Ahuja <leon.ahuja@tandm.ai>
2023-09-19 21:55:59 +1000

    init

---
+++ Scripts/Tools/Singleton.cs
@@ -0,0 +1,13 @@
+
+using Godot;
+using System;
+public partial class Singleton<T> : Node where T : Node
+{
+	public static T _ { get; private set; }
+
+	public override void _Ready()
+	{
+		_ = this as T;
+		GD.Print("Running base ready");
+	}
+}

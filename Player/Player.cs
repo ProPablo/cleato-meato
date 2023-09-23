87320562eac165ae381e4d104f9aa8701539c884
Nyettus <112868686+Nyettus@users.noreply.github.com>
2023-09-23 12:57:17 +1000

    - Basic camera controls

--- Player/Player.cs
+++ Player/Player.cs
@@ -1,42 +1,77 @@
 using Godot;
 using System;
+using System.Diagnostics;
 
 public partial class Player : CharacterBody3D
 {
-	public const float Speed = 5.0f;
-	public const float JumpVelocity = 4.5f;
+    public const float Speed = 5.0f;
+    public const float JumpVelocity = 4.5f;
 
-	// Get the gravity from the project settings to be synced with RigidBody nodes.
-	public float gravity = ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle();
+    public float MouseSenstivity = 0.25f;
+	public float upperBounds = 70f;
+	public float lowerBounds = 80f;
+    private Node3D cameraPivot;
+    // Get the gravity from the project settings to be synced with RigidBody nodes.
+    public float gravity = ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle();
 
-	public override void _PhysicsProcess(double delta)
-	{
-		Vector3 velocity = Velocity;
+    public override void _Ready()
+    {
+        Initialise();
+    }
 
-		// Add the gravity.
-		if (!IsOnFloor())
-			velocity.Y -= gravity * (float)delta;
+    public override void _PhysicsProcess(double delta)
+    {
+        Movement(delta);
+    }
 
-		// Handle Jump.
-		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
-			velocity.Y = JumpVelocity;
+    private void Initialise()
+    {
+        cameraPivot = GetNode<Node3D>("Pivot");
+        Input.MouseMode = Input.MouseModeEnum.Captured;
+    }
 
-		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 inputDir = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
-		if (direction != Vector3.Zero)
-		{
-			velocity.X = direction.X * Speed;
-			velocity.Z = direction.Z * Speed;
-		}
-		else
-		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-			velocity.Z = Mathf.MoveToward(Velocity.Z, 0, Speed);
-		}
+    public override void _Input(InputEvent @event)
+    {
+		//Mouse Controls
+        if (@event is InputEventMouseMotion eventMouseMotion)
+        {
+            Rotate(Vector3.Up, -Mathf.DegToRad(eventMouseMotion.Relative.X * MouseSenstivity));
+            float desiredRotation = Mathf.Clamp(cameraPivot.Rotation.X - Mathf.DegToRad(eventMouseMotion.Relative.Y * MouseSenstivity),
+			-Mathf.DegToRad(upperBounds),
+			Mathf.DegToRad(lowerBounds));
+            cameraPivot.Rotation = new Vector3(desiredRotation, cameraPivot.Rotation.Y, cameraPivot.Rotation.Z);
+        }
+    }
+    private void Movement(double delta)
+    {
+        Vector3 velocity = Velocity;
 
-		Velocity = velocity;
-		MoveAndSlide();
-	}
+        // Add the gravity.
+        if (!IsOnFloor())
+            velocity.Y -= gravity * (float)delta;
+
+        // Handle Jump.
+        if (Input.IsActionJustPressed("Jump") && IsOnFloor())
+            velocity.Y = JumpVelocity;
+
+        // Get the input direction and handle the movement/deceleration.
+        // As good practice, you should replace UI actions with custom gameplay actions.
+        Vector2 inputDir = Input.GetVector("moveLeft", "moveRight", "moveForward", "moveBackward");
+        Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
+        if (direction != Vector3.Zero)
+        {
+            velocity.X = direction.X * Speed;
+            velocity.Z = direction.Z * Speed;
+        }
+        else
+        {
+            velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+            velocity.Z = Mathf.MoveToward(Velocity.Z, 0, Speed);
+        }
+
+        Velocity = velocity;
+        MoveAndSlide();
+    }
+
+
 }
